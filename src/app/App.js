d10e40b935f9c7fbe8f293338dc12b7dc1975565
Maru0214 <112690229+Maru0214@users.noreply.github.com>
2022-10-26 23:44:05 +0900

    20221026

---
+++ src/app/App.js
@@ -0,0 +1,72 @@
+
+import main from  '../img/mainvisual.jpg';
+import tyari1 from  '../img/bicycle1.jpg';
+import tyari2 from  '../img/bicycle2.jpg';
+import tyari3 from  '../img/bicycle3.jpg';
+
+import './App.css';
+
+function App() {
+  return (
+    
+     <body>
+   <header>
+    <nav>
+      <ul>
+        <li>
+
+        </li>
+      </ul>
+    </nav>
+   </header>
+   <main>
+     <div>
+     <img src={main}/>
+     </div>
+     <section>
+      <h2>
+      Bicyce  
+      </h2>
+      <div>
+        <h3>
+          aaa
+        </h3>
+        <p>
+          text
+        </p>
+
+ </div>
+     </section>
+      <h2>
+aaa
+      </h2>
+      <ul>
+        <li>
+         <h3>
+aaaa
+         </h3>
+        </li>
+         <h3>
+aaaa
+         </h3>
+        <li>
+        </li>
+         <h3>
+aaaa
+         </h3>
+        <li>
+
+         </li>
+      </ul>
+    _<section>
+
+    </section>
+   </main>
+   <footer></footer>
+      
+     </body>
+  
+  );
+}
+
+export default App;
